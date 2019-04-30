ad49b4c1ca9fed73eeb305d818c014d141ac99f9
Isu Ahn <clat.dev@gmail.com>
2019-04-30 22:35:24 +0900

    add 2 packages; edit babel and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HTMLWebpackPlugin = require("html-webpack-plugin");
 const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: "./src/App.js",
+  entry: ["@babel/polyfill", "./src/App.js"],
   module: {
     rules: [
       {
